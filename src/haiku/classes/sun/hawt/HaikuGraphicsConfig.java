79c7a92a14873e3b9d2b603608820a205acd4475
Hamish Morrison <hamishm53@gmail.com>
2015-05-23 21:58:13 +0100

    HaikuGraphicsConfig: implement LWGraphicsConfig interface

--- src/haiku/classes/sun/hawt/HaikuGraphicsConfig.java
+++ src/haiku/classes/sun/hawt/HaikuGraphicsConfig.java
@@ -28,9 +28,12 @@ package sun.hawt;
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.*;
+import sun.awt.image.*;
 import sun.java2d.SurfaceData;
+import sun.lwawt.*;
 
-public class HaikuGraphicsConfig extends GraphicsConfiguration {
+public class HaikuGraphicsConfig extends GraphicsConfiguration
+        implements LWGraphicsConfig {
     private final HaikuGraphicsDevice device;
     private ColorModel colorModel;
 
@@ -90,5 +93,80 @@ public class HaikuGraphicsConfig extends GraphicsConfiguration {
         double xscale = device.getXResolution() / 72.0;
         double yscale = device.getYResolution() / 72.0;
         return new AffineTransform(xscale, 0.0, 0.0, yscale, 0.0, 0.0);
+    }
+
+    @Override
+    public int getMaxTextureWidth() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public int getMaxTextureHeight() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public void assertOperationSupported(final int numBuffers,
+                                         final BufferCapabilities caps)
+            throws AWTException {
+        // Assume this method is never called with numBuffers != 2, as 0 is
+        // unsupported, and 1 corresponds to a SingleBufferStrategy which
+        // doesn't depend on the peer. Screen is considered as a separate
+        // "buffer".
+        if (numBuffers != 2) {
+            throw new AWTException("Only double buffering is supported");
+        }
+        if (caps.getFlipContents() == BufferCapabilities.FlipContents.PRIOR) {
+            throw new AWTException("FlipContents.PRIOR is not supported");
+        }
+    }
+
+    @Override
+    public Image createBackBuffer(final LWComponentPeer<?, ?> peer) {
+        final Rectangle r = peer.getBounds();
+        // It is possible for the component to have size 0x0, adjust it to
+        // be at least 1x1 to avoid IAE
+        final int w = Math.max(1, r.width);
+        final int h = Math.max(1, r.height);
+        return createCompatibleImage(w, h);
+    }
+
+    @Override
+    public void destroyBackBuffer(final Image backBuffer) {
+        if (backBuffer != null) {
+            backBuffer.flush();
+        }
+    }
+
+    @Override
+    public void flip(final LWComponentPeer<?, ?> peer, final Image backBuffer,
+                     final int x1, final int y1, final int x2, final int y2,
+                     final BufferCapabilities.FlipContents flipAction) {
+        final Graphics g = peer.getGraphics();
+        try {
+            g.drawImage(backBuffer, x1, y1, x2, y2, x1, y1, x2, y2, null);
+        } finally {
+            g.dispose();
+        }
+        if (flipAction == BufferCapabilities.FlipContents.BACKGROUND) {
+            final Graphics2D bg = (Graphics2D) backBuffer.getGraphics();
+            try {
+                bg.setBackground(peer.getBackground());
+                bg.clearRect(0, 0, backBuffer.getWidth(null),
+                             backBuffer.getHeight(null));
+            } finally {
+                bg.dispose();
+            }
+        }
+    }
+
+    @Override
+    public Image createAcceleratedImage(Component target,
+                                        int width, int height)
+    {
+        ColorModel model = getColorModel(Transparency.OPAQUE);
+        WritableRaster wr = model.createCompatibleWritableRaster(width, height);
+        return new OffScreenImage(target, model, wr,
+                                  model.isAlphaPremultiplied());
     }
 }
