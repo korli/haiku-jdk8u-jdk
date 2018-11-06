8b2ee2a315535242335aeb004007fc977a76472b
Jerome Duval <jerome.duval@gmail.com>
2018-11-06 22:06:07 +0100

    Port change 8163181 to Haiku
    8163181: Further improvements for Unix NetworkInterface native implementation

--- src/solaris/native/java/net/net_util_md.c
+++ src/solaris/native/java/net/net_util_md.c
@@ -1667,7 +1667,7 @@ NET_Wait(JNIEnv *env, jint fd, jint flags, jint timeout)
     return timeout;
 }
 
-#if !defined(__solaris__)
+#if !defined(__solaris__) && !defined(__HAIKU__)
 long NET_GetCurrentTime() {
     struct timeval time;
     gettimeofday(&time, NULL);
